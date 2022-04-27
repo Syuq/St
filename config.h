d79697d8d2425299d25e7de24898554d6abbfbbf
Syuq <tatd.wine@gmail.com>
2022-04-27 10:04:48 +0700

    chore: modified config.h st

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:pixelsize=12:antialias=true:autohint=true";
-static char *font2[] = { "JetBrainsMono Nerd Font:pixelsize=10:antialias=true:autohint=true" };
+static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
+static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
